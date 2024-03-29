693d0b77ae18200bba7d0b65941b4ad094d39971
Lokesh Krishna Cherukuri <lci2022015@iiitl.ac.in>
2024-03-30 03:13:33 +0530

    created navbar

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const page = () => {
+const PersonalRoom = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
         <h1 className='text-3xl font-bold '>
-            Home
+        PersonalRoom
         </h1>
     </section>
   )
 }
 
-export default page
+export default PersonalRoom
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      NAVBAR
+   <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10' >
+    <Link href='/' className=' flex items-center gap-1 '>
+        <Image src="/icons/logo.svg" width={32} height={32} alt='meet-pro logo' className='max-sm:size-10' />
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>meet pro</p>
+    </Link>
+
+    <div className='flex-between gap-5'>
+    {/* clerk user management */}
     </div>
+    
+   </nav>
   )
 }
 
