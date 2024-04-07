85e68dd172158b7d7bd989a3d845a5bee6ff3a69
Lokesh Krishna Cherukuri <lci2022015@iiitl.ac.in>
2024-04-07 18:00:46 +0530

    showing the participents in  meeting room

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,10 +1,33 @@
+'use client'
+import { PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
 import React from 'react'
+import { useState } from 'react'
+type CallLayoutType = 'grid' | 'speaker-right' | 'speaker-left'
 
 const MeetingRoom = () => {
+    const [layout, setLayout] = useState<CallLayoutType>('speaker-left')
+    const CallLayout = ()=>{
+        switch(layout){
+            case 'grid':
+                return <PaginatedGridLayout/>
+            case 'speaker-right': 
+            return <SpeakerLayout participantsBarPosition="left"/>
+            default :
+                return <SpeakerLayout participantsBarPosition="right"/>
+            
+        }
+    }
   return (
-    <div>
-      Meeting room
-    </div>
+    <section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
+        <div className='relative flex size-full items-center justify-center'>
+            <div className='flex size-full max-w-[1000px] items center'>
+                
+<CallLayout/>
+            </div>
+
+        </div>
+ 
+    </section>
   )
 }
 
