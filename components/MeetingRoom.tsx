f9999531a8c6735038971499b4937153408479d9
Lokesh Krishna Cherukuri <lci2022015@iiitl.ac.in>
2024-04-07 18:29:40 +0530

    changed the ui of meeting room by installing stream theme from docs

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,34 +1,49 @@
-'use client'
-import { PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
-import React from 'react'
-import { useState } from 'react'
-type CallLayoutType = 'grid' | 'speaker-right' | 'speaker-left'
+"use client";
+import { cn } from "@/lib/utils";
+import {
+    CallControls,
+  CallParticipantListing,
+  CallParticipantsList,
+  PaginatedGridLayout,
+  SpeakerLayout,
+} from "@stream-io/video-react-sdk";
+import React from "react";
+import { useState } from "react";
+type CallLayoutType = "grid" | "speaker-right" | "speaker-left";
 
 const MeetingRoom = () => {
-    const [layout, setLayout] = useState<CallLayoutType>('speaker-left')
-    const CallLayout = ()=>{
-        switch(layout){
-            case 'grid':
-                return <PaginatedGridLayout/>
-            case 'speaker-right': 
-            return <SpeakerLayout participantsBarPosition="left"/>
-            default :
-                return <SpeakerLayout participantsBarPosition="right"/>
-            
-        }
+  const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
+  const [showParticipants, setShowParticipants] = useState(true);
+  const CallLayout = () => {
+    switch (layout) {
+      case "grid":
+        return <PaginatedGridLayout />;
+      case "speaker-right":
+        return <SpeakerLayout participantsBarPosition="left" />;
+      default:
+        return <SpeakerLayout participantsBarPosition="right" />;
     }
+  };
   return (
-    <section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
-        <div className='relative flex size-full items-center justify-center'>
-            <div className='flex size-full max-w-[1000px] items center'>
-                
-<CallLayout/>
-            </div>
-
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className="flex size-full max-w-[1000px] items center">
+          <CallLayout />
         </div>
- 
-    </section>
-  )
-}
+        <div
+          className={cn("h-[calc(100vh-86px)] hidden ml-2", {
+            "show-block": showParticipants,
+          })}
+        >
+          <CallParticipantsList onClose={() => setShowParticipants(false)} />
+        </div>
+      </div>
+      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+        <CallControls/>
 
-export default MeetingRoom
+      </div>
+    </section>
+  );
+};
+
+export default MeetingRoom;
