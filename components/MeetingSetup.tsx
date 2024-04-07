0802643f5a6ad980aa70ad9b2735f429c2b24927
Lokesh Krishna Cherukuri <lci2022015@iiitl.ac.in>
2024-04-07 17:03:34 +0530

    camera and microphone setup completed

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+'use client'
+import { DeviceSettings, VideoPreview, useCall } from '@stream-io/video-react-sdk'
+import React, { useEffect, useState } from 'react'
+
 
 const MeetingSetup = () => {
+    const [isMicCamToggleOn, setIsMicCamToggleOn] = useState(false)
+
+    const call = useCall()
+    if(!call){
+        throw new Error('use call must be used within stream call component')
+    }
+    useEffect(()=>{
+        if(isMicCamToggleOn){
+            call?.microphone.disable()
+            call?.camera.disable()
+        } else{
+            call?.microphone.enable()
+            call?.camera.enable()
+        }
+
+    },[isMicCamToggleOn , call?.microphone,call?.camera])
   return (
-    <div>
-      Meeting setup
+    <div className='flex h-screenw-full flex-col items-center justify-center gap-3 text-white'>
+        <h1 className='text-2xl font-bold'>
+            Setup
+        </h1>
+        <VideoPreview />
+        <div className='flex h-16 items-center justify-center gap-3'>
+            <label className='flex items-center justify-center gap-2 font-medium'>
+
+            
+            <input type='checkbox' checked={isMicCamToggleOn} onChange={(e)=>setIsMicCamToggleOn(e.target.checked)}/>
+            join with mic and camera off
+            </label>
+            <DeviceSettings/>
+        </div>
+     
     </div>
   )
 }
