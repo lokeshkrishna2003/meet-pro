a376218589a3451d8d284d646f179b4ae114e6b3
Lokesh Krishna Cherukuri <lci2022015@iiitl.ac.in>
2024-04-09 03:54:31 +0530

    fetched the upcoming meeting calls

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,9 +1,11 @@
+//@ts-nocheck
 'use client'
 import { UseGetCalls } from '@/hooks/UseGetCalls'
 import { Call, CallRecording } from '@stream-io/video-react-sdk'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 import MeetingCard from './MeetingCard'
+import Loader from './Loader'
 
 
 const CallList = ({type}:{type : 'ended' | 'upcoming' | 'recordings'}) => {
@@ -37,10 +39,24 @@ const CallList = ({type}:{type : 'ended' | 'upcoming' | 'recordings'}) => {
     }
     const calls = getCalls()
     const noCallMessage = getNoCallMessage()
+    if(isLoading){
+        return <Loader/>
+    }
   return (
     <div className='grid grid-cols-1 gap-5 xl:grid-cols-2'>
       {calls && calls.length>0 ? calls.map((meeting:Call|CallRecording)=>(
-        <MeetingCard/>
+        <MeetingCard
+        key={(meeting as Call).id}
+        icon ={type==='ended'?'/icons/previous.svg':type==='upcoming'?'/icons/upcoming.svg':'/icons/Video.svg'}
+        title ={(meeting as Call).state.custom.description.substring(0,20)}
+        date ={(meeting as Call).state.startsAt?.toLocaleString()||(meeting as Call).start_time.toLocaleString()}
+        isPreviousMeeting ={type==='ended'}
+        buttonIcon1 ={type==='recordings'?'/icons/play.svg':undefined}
+        handleClick ={type==='recordings'?()=>router.push(`${meeting.url}`):()=>router.push(`meeting/${meeting.id}`)}
+        link ={type==='recordings'?`${meeting.url}`:`${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meeting.id}`}
+        buttonText =
+        {type==='recordings'?'play':'start'}             
+      />
       )) : (
         <h1>
           {noCallMessage}
