f2fb87c41e94f02597d218de4ebf795575253787
Lokesh Krishna Cherukuri <lci2022015@iiitl.ac.in>
2024-04-09 02:51:14 +0530

    created usegetcalls hook for fetching upcoming calls and ended calls

---
+++ hooks/UseGetCalls.tsx
@@ -0,0 +1,50 @@
+import { useUser } from "@clerk/nextjs"
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk"
+import { error } from "console"
+import { useEffect, useState } from "react"
+
+export const UseGetCalls = ()=>{
+const [Calls,setCalls] = useState<Call[]>([])
+const [isLoading,setisLoading] = useState(false)
+const client = useStreamVideoClient()
+const {user} = useUser()
+
+useEffect(()=>{
+    const loadCalls = async ()=>{
+        if(!client || !user?.id) return
+        setisLoading(true)
+        try{
+            const {calls} = await client.queryCalls({
+                sort:[{
+                    field:'starts_at' ,direction:-1
+                }],
+                filter_conditions:{
+                    starts_at:{$exists:true},
+                    $or : [{created_by_user_id:user.id},
+                    {members:{$in:[user.id]}}]
+                }
+            })
+            setCalls(calls)
+        }
+        catch(err){
+            console.log(err)
+        }
+        finally{
+            setisLoading(false)
+        }
+    }
+    loadCalls()
+
+},[client,user?.id])
+const now = new Date()
+const endedCalls = Calls.filter(({state:{startsAt,endedAt}}:Call)=>{
+    return (startsAt && new Date(startsAt) < now || !!endedAt)
+})
+const upcomingCalls = Calls.filter(({state:{startsAt}}:Call)=>{
+return (startsAt && new Date(startsAt) > now)
+})
+let recordings;
+return {
+    endedCalls, recordings, upcomingCalls , setisLoading
+}
+}
